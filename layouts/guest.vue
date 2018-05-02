0cc013012af678849a861275161fdc1db613717d
Pedro Martín del Campo González <pedro.martindelcampo.gonzalez@gmail.com>
2018-05-02 16:54:40 -0700

    Add CSS

---
+++ layouts/guest.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <GuestNavbar />
+    <nuxt/>
+  </div>
+</template>
+<script>
+import GuestNavbar from '~/components/GuestNavbar.vue'
+export default {
+  components: {
+      GuestNavbar
+  }
+}
+</script>
